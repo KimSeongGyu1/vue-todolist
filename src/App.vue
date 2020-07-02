cd94d6293416ed54be4462ab87302b8282bf9999
kimseonggyu1 <luckykimsk@gmail.com>
2020-07-02 22:14:27 +0900

    feat: 화면 html 구현

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div class="todoapp">
+
+    <h1>TODOS</h1>
+
+    <input
+      class="new-todo"
+      placeholder="할 일을 추가해주세요"
+    >
+
+    <div class="todo-list">
+      <li>
+        <div class="view">
+          <input
+            class="toggle"
+            type="checkbox"
+          >
+          <label class="label">new todo 1</label>
+          <button class="destroy"></button>
+        </div>
+      </li>
+    </div>
+
+    <div class="count-container">
+      <span class="todo-count">
+        1 개
+      </span>
+
+      <ul class="filters">
+        <li>
+          <a>전체보기</a>
+        </li>
+        <li>
+          <a>해야할 일</a>
+        </li>
+        <li>
+          <a>완료한 일</a>
+        </li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
